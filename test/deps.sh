94b138810cc0fa6ec50b4913768dfc1dbd517b2e
Boris Rybalkin <ribalkin@gmail.com>
2023-12-22 14:01:03 +0000

    no image test

--- test/deps.sh
+++ test/deps.sh
@@ -5,6 +5,5 @@ while ! apt-get update; do
   sleep 1
   echo "retry"
 done
-apt-get install -y sshpass openssh-client wget imagemagick
+apt-get install -y sshpass openssh-client wget
 pip install -r requirements.txt
-mx=1000;my=1000;head -c "$((3*mx*my))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- $DIR/images/image-big.png
