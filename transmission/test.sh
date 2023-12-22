f479821d91a23099c0de8846949d50b781db2ee4
Boris Rybalkin <ribalkin@gmail.com>
2023-12-22 11:40:27 +0000

    fix

---
+++ transmission/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PREFIX=${DIR}/../build/snap/transmission
+
+du -d10 -h $PREFIX | sort -h | tail -100
+
+ldd ${PREFIX}/usr/sbin/postfix
+ls -la ${PREFIX}/bin
+${PREFIX}/bin/transmission.sh --help
