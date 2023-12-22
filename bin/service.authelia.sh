da63fd09755cf9efae7efe3faf7527dd87b8d9ad
Boris Rybalkin <ribalkin@gmail.com>
2023-12-22 21:05:06 +0000

    config

--- bin/service.authelia.sh
+++ bin/service.authelia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-exec ${DIR}/authelia/bin/authelia.sh --config /var/snap/transmission/config/authelia.yml
+exec ${DIR}/authelia/bin/authelia.sh --config /var/snap/transmission/current/config/authelia.yml
