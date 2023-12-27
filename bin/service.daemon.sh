4b34033cbda8356902550cee796edec5f5473296
Boris Rybalkin <ribalkin@gmail.com>
2023-12-27 16:37:44 +0000

    logs

--- bin/service.daemon.sh
+++ bin/service.daemon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-exec ${DIR}/transmission/bin/transmission.sh -g /var/snap/transmission/current/config/transmission --no-auth -f --log-level=error
+exec ${DIR}/transmission/bin/transmission.sh -g /var/snap/transmission/current/config/transmission --no-auth -f --log-level=debug
