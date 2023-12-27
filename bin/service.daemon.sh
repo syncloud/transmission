b62e9401166198d31aec76dd24e0e354a20053ec
Boris Rybalkin <ribalkin@gmail.com>
2023-12-27 22:34:39 +0000

    unix

--- bin/service.daemon.sh
+++ bin/service.daemon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-exec ${DIR}/transmission/bin/transmission.sh -g /var/snap/transmission/current/config/transmission --rpc-bind-address /var/snap/transmission/current/transmission.socket --no-auth -f --log-level=debug
+exec ${DIR}/transmission/bin/transmission.sh -g /var/snap/transmission/current/config/transmission --no-auth -f --log-level=debug
