796e35450da4254d01932a3d6eb76757de26f7f9
Boris Rybalkin <ribalkin@gmail.com>
2023-12-27 23:05:57 +0000

    fix

--- bin/service.daemon.sh
+++ bin/service.daemon.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export TRANSMISSION_WEB_HOME=${DIR}/transmission/usr/local/share/transmission/public_html
 exec ${DIR}/transmission/bin/transmission.sh -g /var/snap/transmission/current/config/transmission --no-auth -f --log-level=debug
